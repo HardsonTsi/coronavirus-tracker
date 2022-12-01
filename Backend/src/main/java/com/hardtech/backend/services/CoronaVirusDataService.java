2df860a6d19f15db6eb1fa30e8d29bbfe5031df7
TESSI Hardson <hardsontessi2@gmail.com>
2022-12-01 15:14:17 +0100

    csv dependency, enable scheduling

--- Backend/src/main/java/com/hardtech/backend/services/CoronaVirusDataService.java
+++ Backend/src/main/java/com/hardtech/backend/services/CoronaVirusDataService.java
@@ -1,9 +1,13 @@
 package com.hardtech.backend.services;
 
 import jakarta.annotation.PostConstruct;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.io.StringReader;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -18,13 +22,24 @@ public class CoronaVirusDataService {
 
     //get file
     @PostConstruct
+    @Scheduled(cron = "* * 1 * * *")
     public void fetchVirusData() throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(VIRUS_DATA_URL))
                 .build();
         HttpResponse<String> httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(httpResponse.body());
+        //System.out.println(httpResponse.body());
+
+
+        //csv reader
+        StringReader csvBodyReader = new StringReader(httpResponse.body());
+        Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvBodyReader);
+        for (CSVRecord record : records) {
+            String state = record.get("Province/State");
+            System.out.println(state);
+        }
+
     }
 
 
