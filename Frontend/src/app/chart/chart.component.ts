19fbb78c36323449da8e5700c1261fb4912549f5
TESSI Hardson <hardsontessi2@gmail.com>
2022-12-06 18:18:29 +0100

    refactoring chart

--- Frontend/src/app/chart/chart.component.ts
+++ Frontend/src/app/chart/chart.component.ts
@@ -1,52 +1,49 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {LocationStats} from "../classes/location-stats";
+import {LocationStats} from "../classes/locationStats";
 import {Chart} from "chart.js";
 
 @Component({
-  selector: 'chart',
-  templateUrl: './chart.component.html',
-  styleUrls: ['./chart.component.css']
+    selector: 'chart',
+    templateUrl: './chart.component.html',
+    styleUrls: ['./chart.component.css']
 })
 export class ChartComponent implements OnInit {
 
 
-  @Input() locationStats: LocationStats = new LocationStats();
-  @Input() totalCasesPreviousDay: number;
+    @Input() locationStats: LocationStats;
+    @Input() totalCases: number;
+    @Input() totalCasesPreviousDay: number;
 
-  constructor() {
-  }
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
-  ngAfterViewInit(){
-    this.renderChart();
+    ngAfterViewInit() {
+        this.renderChart();
+    }
 
-  }
-
-
-  renderChart() {
-    var myChart = new Chart(`${this.locationStats.country}`, {
-      type: 'pie',
-      data: {
-        labels: [
-          'Nombre total de cas  signalés depuis hier',
-          'Nombre de cas signalés à ce jour',
-          'Nombre de cas signalés hier'
-        ],
-        datasets: [{
-          label: `${this.locationStats.country} | ${this.locationStats.state}`,
-          data: [this.totalCasesPreviousDay, this.locationStats.latestTotalCases, this.locationStats.diffFromPreviousDay],
-          backgroundColor: [
-            'rgb(54, 162, 235)',
-            'rgb(255, 99, 132)',
-            'rgb(255, 205, 86)'
-          ],
-          hoverOffset: 4
-        }]
-      }
-    });
-  }
+    renderChart() {
+        var myChart = new Chart(`${this.locationStats.country}`, {
+            type: 'pie',
+            data: {
+                labels: [
+                    'Nombre de cas signalés à ce jour',
+                    'Nombre de cas signalés hier'
+                ],
+                datasets: [{
+                    label: `${this.locationStats.country} | ${this.locationStats.state}`,
+                    data: [this.locationStats.latestTotalCases, this.totalCasesPreviousDay],
+                    backgroundColor: [
+                        'rgb(255, 99, 132)',
+                        'rgb(255, 205, 86)'
+                    ],
+                    hoverOffset: 4
+                }]
+            }
+        });
+    }
 
 
 }
