2fbbf701c9b2e417c519d4ab0ebd461332091222
TESSI Hardson <hardsontessi2@gmail.com>
2022-12-06 17:54:03 +0100

    header and stats-table component

--- Frontend/src/app/services/corona-virus-data.service.ts
+++ Frontend/src/app/services/corona-virus-data.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {environment} from "../../environments/environment";
 import {Observable} from "rxjs";
-import {LocationStats} from "../classes/location-stats";
+import {LocationStats} from "../classes/locationStats";
 
 @Injectable({
     providedIn: 'root'
